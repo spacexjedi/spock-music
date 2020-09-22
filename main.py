ca1120e215e52c7468980a378450e54dbc453405
Darko Donnie <donni3thedarko@gmail.com>
2020-09-22 18:49:35 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,23 @@
+#fplayer.py 
+
+from flask import Flask, url_for, render_template
+import glob
+import request
+import json 
+
+app = Flask(__name__)
+
+@app.route("/sounds")
+def sounds():
+  music = request.args["music"]
+  return send_file(music, mimetype="audio/mp3")
+
+@app.route("/")
+def home():
+  musiclist = glob.glob("static/musics/*.mp3")
+  musicJ = [{'fileName': mi.split("/")[-1],"fileURL": url_for('sounds', music=mi)}for mi in musiclist]
+  return render_template("home.html", musicJ = musicJ, musicJson=json.dumps(musicJ))
+
+
+if(__name__=="__main__"):
+  app.run()
